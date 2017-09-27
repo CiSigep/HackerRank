000c515d7af77fd09855374be22ef648f5a49f7d
CiSigep <Cisigep@gmail.com>
2017-09-27 09:56:13 -0400

    Create

---
+++ Solutions/Algorithms/Strings/CamelCase/Solution.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+class Solution {
+
+    static void Main(String[] args) {
+        string s = Console.ReadLine();
+        int count = 1;
+        
+        foreach(char c in s)
+            if(Char.IsUpper(c))
+                count++;
+        
+        Console.WriteLine(count);
+    }
+}
