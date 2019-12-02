ec241b9f3468d6b2d74b700efbdf6423e50d1ef1
Jeffrey Freeman <Cisigep@gmail.com>
2019-12-02 14:17:07 -0600

    Create solution.py

---
+++ Solutions/Python/Strings/sWAP cASE/solution.py
@@ -0,0 +1,7 @@
+def swap_case(s):
+    return ''.join(ch.lower() if ch.isupper() else ch.upper() for ch in s)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
